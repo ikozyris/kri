c9bf63d3042dc98bac7eca9718ee6bb7a44eb958
ikozyris <ikozyris1337@gmail.com>
2025-01-30 20:20:32 +0200

    cleanup: use constant gap_buf on non-modifier funcs

--- utils/headers/search.h
+++ utils/headers/search.h
@@ -2,8 +2,8 @@
 #include "io.h"
 #include "key_func.h"
 
-vector<uint> bm_search(gap_buf &buf, const char *str, ushort len);
+vector<uint> bm_search(const gap_buf &buf, const char *str, ushort len);
 // search for str in buf, return vector of <position, color(position's)>
-vector<uint> search_a(gap_buf &buf, const char *str, ushort len);
-uint search_c(gap_buf &buf, const char *str, ushort len);
+vector<uint> search_a(const gap_buf &buf, const char *str, ushort len);
+uint search_c(const gap_buf &buf, const char *str, ushort len);
 void find(const char *str, uchar mode); // wrapper searching
--- utils/headers/sizes.h
+++ utils/headers/sizes.h
@@ -4,8 +4,8 @@
 char hrsize(size_t bytes, char *dest, ushort dest_cpt); // format bytes
 uint memusg(); // memory usage (PSS) of kri in kilobytes
 uint whereis(const char *str, char ch); // position of ch in str (wrapper for strch)
-long calc_offset_dis(uint dx, gap_buf &buf); // offset until displayed character
-long calc_offset_act(uint pos, uint i, gap_buf &buf); // offset from i until byte pos
-uint dchar2bytes(uint dx, uint from, gap_buf &buf); // how many bytes are dx displayed characters
+long calc_offset_dis(uint dx, const gap_buf &buf); // offset until displayed character
+long calc_offset_act(uint pos, uint i, const gap_buf &buf); // offset from i until byte pos
+uint dchar2bytes(uint dx, uint from, const gap_buf &buf); // how many bytes are dx displayed characters
 uint mbcnt(const char *str, uint len); // count multi-byte characters in string
 uint prevdchar(); // left arrow on end of tab; update offset and move cursor
