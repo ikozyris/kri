d73f46a6bf06158d8c64e8fbe4296acd1bdcdd9e
ikozyris <ikozyris1337@gmail.com>
2025-03-25 22:22:31 +0200

    optim: multi-threading over lines
    WIP

--- utils/headers/search.h
+++ utils/headers/search.h
@@ -4,5 +4,7 @@
 
 // search for str in buf, return vector of occurences
 vector<uint> search_a(const gap_buf &buf, const char *str, ushort len);
+vector<vector<uint>> search_la(uint from, uint to, const char *str, ushort len); // use above to search on multiple lines
 uint search_c(const gap_buf &buf, const char *str, ushort len); // same as above, but only counts
-void find(const char *str, uint from, uint to, char mode); // wrapper searching
+ulong search_lc(uint from, uint to, const char *str, ushort len);
+void find(const char *str, uint from, uint to, char mode); // wrapper for search_l
