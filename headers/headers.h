1292a55cb01c3f943b1afe681951feff68576338
ikozyris <ikozyris1337@gmail.com>
2025-04-07 19:56:59 +0300

    cleanup strings/comments

--- headers/headers.h
+++ headers/headers.h
@@ -7,8 +7,8 @@
 #include <thread>
 #include <bit>	// for __bit_ceil (cntlz)
 #include <signal.h> // for pause in command suspend
-typedef unsigned long ulong;
-typedef unsigned int uint;
-typedef unsigned short ushort;
-typedef unsigned char uchar;
+typedef uint64_t ulong;
+typedef uint32_t uint;
+typedef uint16_t ushort;
+typedef uint8_t uchar;
 using namespace std;
