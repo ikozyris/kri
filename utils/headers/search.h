fde0a182362a506ec5a58d655e52f98c9e3fe96d
ikozyris <ikozyris1337@gmail.com>
2025-09-30 20:27:03 +0300

    fix: search
    There is an off-by-one error in automatically cutting line when matchx > maxx.
    Replace is not fixed yet

--- utils/headers/search.h
+++ utils/headers/search.h
@@ -1,11 +1,8 @@
 #pragma once
 #include "io.h"
 #include "key_func.h"
+#include "../../ds/headers/dynarray.h"
 
-// search for str in buf, return vector of occurences
-vector<uint> search_a(const gap_buf &buf, const char *str, ushort len);
-vector<vector<uint>> search_la(uint from, uint to, const char *str, ushort len); // use above to search on multiple lines
-uint search_c(const gap_buf &buf, const char *str, ushort len); // same as above, but only counts
-uint search_lc(uint from, uint to, const char *str, ushort len);
-vector<uint> mt_search(const gap_buf &buf, char ch, bool append);
+void search_la(uint from, uint to);
+void search_lc(uint from, uint to);
 void find(const char *str, uint from, uint to, char mode); // wrapper for search_l
